aa91df301281c72a149944b4a050df16c982830a
Arthur Santana <arthur.alexandre330@gmail.com>
2022-04-02 14:30:42 -0300

    final commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const darkModeToggleButton = document.querySelector('button#dark-mode-toggle')
+const darkModeToggleButtonIcon = document.querySelector('button#dark-mode-toggle>img.icon')
+const body = document.querySelector('body')
+
+toggleDarkMode = () => {
+	body.classList.toggle('dark-mode')
+
+	if (body.classList.contains('dark-mode')) {
+		darkModeToggleButtonIcon.src = './assets/img/sun-solid.svg'
+	} else {
+		darkModeToggleButtonIcon.src = './assets/img/moon-solid.svg'
+	}
+	darkModeToggleButtonIcon.classList.toggle('white')
+}
